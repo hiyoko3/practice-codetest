6aad2b63c122af92f39fe5135995b276e2e753f6
hiyoko3 <16721102+hiyoko3@users.noreply.github.com>
2022-11-03 18:49:33 +0900

    add gitignore

--- at-coder/abc/abc-151/A.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-int main() {
-	char C;
-	cin >> C;
-	cout << ++C << endl;
-}
--- at-coder/abc/abc-169/C.py
+++
@@ -1,6 +0,0 @@
-from math import floor
-from fractions import Fraction
-a, b = input().split()
-a = int(a)
-b = Fraction(b)
-print(floor(a * b))
